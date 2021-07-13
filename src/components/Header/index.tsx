dfc8c6849b0590e92cf82e3767558075e546d60b
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2021-07-13 03:37:24 -0300

    removed cart when logoff

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import Link from 'next/link';
 import { BoxCart, Container } from './styles';
-import { FaShoppingCart } from 'react-icons/fa'
+import { FaShoppingCart } from 'react-icons/fa';
 import { SignInButton } from '../SignInButton';
+import { useSession } from 'next-auth/client';
 
 interface HeaderProps {
   onOpenLoginModal: () => void;
 }
 
 export function Header({ onOpenLoginModal }: HeaderProps) {
+  const [session] = useSession();
+
   return (
     <Container>
       <div>
@@ -34,9 +37,11 @@ export function Header({ onOpenLoginModal }: HeaderProps) {
           <li>
             <SignInButton onOpenLoginModal={onOpenLoginModal} />
           </li>
-          <BoxCart>
-            <FaShoppingCart />
-          </BoxCart>
+          {session && (
+            <BoxCart>
+              <FaShoppingCart />
+            </BoxCart>
+          )}
         </ul>
       </nav>
     </Container>
